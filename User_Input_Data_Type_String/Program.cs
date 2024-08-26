c0b2aa1ddeee6b7478147feaaf51467dc1f432b3
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-25 22:22:11 -0500

    Return user input

--- User_Input_Data_Type_String/Program.cs
+++ User_Input_Data_Type_String/Program.cs
@@ -1,5 +1,10 @@
 ﻿// Pints whatever is in the brackets to the console
-Console.WriteLine("Example");
+Console.WriteLine("Enter Something: ");
 
 // Get user input
+// Variable with the name "userInput"
+// and the datatype 'String'
 string userInput = Console.ReadLine();
+// Print out the user input
+Console.WriteLine("You entered " + userInput);
+Console.ReadKey();
