cb29667333abc5bcad1663797cb8d90dc7f9268f
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-27 18:32:31 -0500

    Build foundation

--- Addition_Calculator/add_calc/Program.cs
+++ Addition_Calculator/add_calc/Program.cs
@@ -1,9 +1,18 @@
 ﻿// datatype variableName = initial value;
 Console.WriteLine("Enter a whole number: ");
-int myNumber = 0;
+//int myNumber = 0;
 
-string userInput = Console.ReadLine();
-myNumber = int.Parse(userInput);
-Console.WriteLine($"You entered {myNumber}.");
+//string userInput = Console.ReadLine();
+//myNumber = int.Parse(userInput);
+//Console.WriteLine($"You entered {myNumber}.");
 
+// Actual addition calculator
 
+int firstNumber = 0;
+int secondNumber = 0;
+
+// Store the first number
+string firstNumberInput = Console.ReadLine();
+// Parse the first number
+firstNumber = int.Parse(firstNumberInput);
+string secondNumberInput = Console.ReadLine();
