656c4842dab50eefe278d8218c3edf1604d4893a
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-28 19:57:31 -0500

    Add WriteLine statements for examples

--- Conversion_helpers/parse_convert/Program.cs
+++ Conversion_helpers/parse_convert/Program.cs
@@ -3,9 +3,10 @@
 //Parse
 string numberString = "123";
 int result = int.Parse(numberString);
+Console.WriteLine(result);
 
 //Convert
 string myBoolString = "true";
 bool myBool = Convert.ToBoolean(myBoolString);
-
+Console.WriteLine($"myBool is {myBool}");
 Console.ReadKey();
