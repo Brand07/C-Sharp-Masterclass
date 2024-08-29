27b8ec24aa5c7f7b95af01f8fc04d2a1139320eb
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-28 19:41:23 -0500

    Add basic idea(s)

---
+++ implicit_conversion/implicit_conversion/Program.cs
@@ -0,0 +1,14 @@
+﻿//implicit conversion
+
+int myInt = 13;
+//Double holds more than an int
+double myDouble = myInt;
+//Long can hold more data than an a double
+long myLong = myInt;
+
+/*
+ * You can go up in size but not down
+ */
+
+
+Console.ReadKey();
