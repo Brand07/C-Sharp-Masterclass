d2506a576131d2678bf655df39b4c9c5e46c5cb4
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-28 19:56:18 -0500

    Add examples

---
+++ Conversion_helpers/parse_convert/Program.cs
@@ -0,0 +1,11 @@
+﻿//Conversion helpers Parse and Convert
+
+//Parse
+string numberString = "123";
+int result = int.Parse(numberString);
+
+//Convert
+string myBoolString = "true";
+bool myBool = Convert.ToBoolean(myBoolString);
+
+Console.ReadKey();
