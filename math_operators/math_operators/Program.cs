b5dc470ea641afb9d69d2bc9258fac4ec60f4be5
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-29 20:10:04 -0500

    Add Division example

--- math_operators/math_operators/Program.cs
+++ math_operators/math_operators/Program.cs
@@ -12,3 +12,5 @@ Console.WriteLine("Subtraction of num1 - num2 = " + (num1 - num2));
 //Multiplication
 Console.WriteLine("Multiply num1 & num2 = " + (num1 * num2));
 
+//Division
+Console.WriteLine("Divide num1 by num2 = " + (num1 / (double)(num2)));
