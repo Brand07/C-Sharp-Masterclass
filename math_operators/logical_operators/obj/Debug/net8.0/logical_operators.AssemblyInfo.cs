1ec43b265461ea7c321e839b680384c8a3faf938
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 21:14:13 -0500

    Add other project files

--- math_operators/logical_operators/obj/Debug/net8.0/logical_operators.AssemblyInfo.cs
+++ math_operators/logical_operators/obj/Debug/net8.0/logical_operators.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("logical_operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02cef7fe1099fee501dd53d8f9a57dfd14bc18ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08b44894240ef654a87a1dabda688153d104b05c")]
 [assembly: System.Reflection.AssemblyProductAttribute("logical_operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("logical_operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
