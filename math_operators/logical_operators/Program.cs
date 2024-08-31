02cef7fe1099fee501dd53d8f9a57dfd14bc18ea
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 20:42:02 -0500

    Add bool and if examples

--- math_operators/logical_operators/Program.cs
+++ math_operators/logical_operators/Program.cs
@@ -4,3 +4,12 @@
  * 2 - Comparative Operators such as equal to, greater than, less than, etc
  * 3 - Combining Expressions - combine boolean (true or false) expressions
 */
+// Boolean statement
+bool isRainy = true;
+bool isSunny = true;
+
+// If statements
+if (isRainy)
+{
+ Console.WriteLine("Grab an umbrella!");
+}
