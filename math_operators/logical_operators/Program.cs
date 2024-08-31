a339108ced2adc48600d30ddfe1983df10055aa9
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 20:37:49 -0500

    Add comments explaining concepts

--- math_operators/logical_operators/Program.cs
+++ math_operators/logical_operators/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿//Introduction to Logic in C#
+/*
+ * 1 - Logical Operators such as AND, OR, NOT
+ * 2 - Comparative Operators such as equal to, greater than, less than, etc
+ * 3 - Combining Expressions - combine boolean (true or false) expressions
+*/
