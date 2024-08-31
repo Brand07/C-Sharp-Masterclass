e8455c254bd982707a9dfc8fa99d47bf346fb9b5
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 20:46:57 -0500

    Add examples for each if statement

--- math_operators/logical_operators/Program.cs
+++ math_operators/logical_operators/Program.cs
@@ -5,11 +5,27 @@
  * 3 - Combining Expressions - combine boolean (true or false) expressions
 */
 // Boolean statement
-bool isRainy = true;
+bool isRainy = false;
 bool isSunny = true;
 
-// If statements
-if (isRainy)
+// If statement using AND
+if (isRainy && isSunny)
 {
- Console.WriteLine("Grab an umbrella!");
+ Console.WriteLine("Its going to be humid!");
 }
+// If statement using OR
+if (isRainy || isSunny)
+{
+ Console.WriteLine("There will certainly be a mixture of weather today.");
+}
+// If statement using NOT
+if (!isRainy)
+{
+ Console.WriteLine("There will be no rain today.");
+}
+
+
+// Logical Operators:
+// AND &&
+// OR ||
+// NOT !
