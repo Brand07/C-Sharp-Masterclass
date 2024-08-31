c0f494f8514aa8046b1d0ac019c348b33492edca
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-31 14:52:55 -0500

    Add equality operators

--- elseif/ConsoleApp1/Program.cs
+++ elseif/ConsoleApp1/Program.cs
@@ -1,11 +1,23 @@
-﻿int age = 8;
+﻿//Equality operators
 
-if (age >= 18)
+int num1 = 0;
+int num2 = 0;
+
+//Checks for equality
+bool isEqual = num1 == num2; // true
+//Checks for inequality
+bool isNotEqual = num1 != num2; // false
+
+
+int age = 16;
+bool isWithParents = false;
+
+if (age >= 18 && isWithParents)
 {
-    Console.WriteLine("Go Vote!");
-} else if (age >= 6)
+    Console.WriteLine("Go to a school party with your parents.");
+} else if (age <= 18)
 {
-    Console.WriteLine("Go to School!");
+    Console.WriteLine("Party by yourself.");
 } else if (age < 6)
 {
     Console.WriteLine("Be a kid!");
