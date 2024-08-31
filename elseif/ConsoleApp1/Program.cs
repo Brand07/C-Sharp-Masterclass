2c023604e04fb4999b755b0c5a40586bc6f5cdac
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-31 14:43:38 -0500

    Add Example of using else if

---
+++ elseif/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿int age = 8;
+
+if (age >= 18)
+{
+    Console.WriteLine("Go Vote!");
+} else if (age >= 6)
+{
+    Console.WriteLine("Go to School!");
+} else if (age < 6)
+{
+    Console.WriteLine("Be a kid!");
+}
