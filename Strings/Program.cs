9ee258c917d5fa31aba80283de5bbe229fbe2971
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-08-30 20:23:28 -0500

    Add Escape character example

--- Strings/Program.cs
+++ Strings/Program.cs
@@ -10,7 +10,7 @@ Console.WriteLine(myName);
 // Char datatype - can only contain one character 
 // Only characters in UTF-16 format
 // Single quotes
-char myFavoriteCharacter = 'a';
+//char myFavoriteCharacter = 'a';
 
 //String formatting and interpolation
 int num = 10;
@@ -23,5 +23,12 @@ Console.WriteLine("The number is " + num);
 
 //String formatting
 Console.WriteLine("The number is {0} and the price is {1}. Your salesperson is {2}.", num, price, name);
+
+// Special & escape characters
+//string s1 = "this is a string with a slash / and a colon: ";
+//Console.WriteLine(s1);
+// Quoted text in strings has to be escaped with two back slashes.
+string s1 = "this is a \"string\" with a slash / and a colon: ";
+Console.WriteLine(s1);
 
 
