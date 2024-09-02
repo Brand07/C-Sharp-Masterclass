b822551030cfef0da92376accb10ccf17f575707
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-02 00:45:18 -0500

    Add basic questions

--- quiz_app/quiz_app/Program.cs
+++ quiz_app/quiz_app/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string questionOne = "What is the capital of Germany: ";
+string answerOne = "Berlin";
+
+string questionTwo = "What is 2+2?";
+string answerTwo = "4";
+
+string questionThree = "What color do you get by mixing blue and yellow?";
+string answerThree = "Green";
