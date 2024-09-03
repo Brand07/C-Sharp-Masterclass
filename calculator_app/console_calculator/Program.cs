6aae870700f1aac42aee15c83bb1b6d9fb602d35
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-02 20:18:54 -0500

    Add simple addition/subtraction/multiplication/division calc

--- calculator_app/console_calculator/Program.cs
+++ calculator_app/console_calculator/Program.cs
@@ -1,30 +1,4 @@
-﻿// //Create an instance of the random class
-// Random random = new Random();
-// //1,11 because the last number is exclusive
-// //This will generate a random number between 1 and 10.
-// int randomNumber = random.Next(1, 11);
-//
-// //Prompt user for a number
-// Console.WriteLine("Guess the number: ");
-// //Store the user's reponse on the inputString
-// string inputString = Console.ReadLine();
-// int num1 = 0;
-//
-// bool isNumber = int.TryParse(inputString, out num1);
-//
-// if (isNumber)
-// {
-// 	if (num1 == randomNumber)
-// 	{
-// 		Console.WriteLine("You guessed the correct number!");
-// 	}
-// 	else
-// 	{
-// 		Console.WriteLine($"Sorry, do you didn't guess the correct number. The random number was {randomNumber} ");
-// 	}
-// }
-
-Console.WriteLine("Enter your first number: ");
+﻿Console.WriteLine("Enter your first number: ");
 string num1 = Console.ReadLine();
 Console.WriteLine("Enter your second number: ");
 string num2 = Console.ReadLine();
