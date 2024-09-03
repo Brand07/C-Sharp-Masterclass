2329d0b41c7ab23eb912547b0d2b4a47c7f8b149
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-02 19:16:45 -0500

    Add other project files

--- quiz_app/quiz_app/obj/Debug/net8.0/quiz_app.AssemblyInfo.cs
+++ quiz_app/quiz_app/obj/Debug/net8.0/quiz_app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quiz_app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51764741046a6799b9664328de3306be87575c44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8cff5cbe2e9a1b79e5950edd22694aaec325a751")]
 [assembly: System.Reflection.AssemblyProductAttribute("quiz_app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quiz_app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
