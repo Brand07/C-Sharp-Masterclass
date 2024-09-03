6c249e45712b044647e8bdf46f7d0d7ecf8b2972
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-02 19:22:00 -0500

    Trying TryParse

---
+++ try_parse/try_parse/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Give me a number");
+string inputString = Console.ReadLine(); 
+int num1;
+
+int.TryParse(inputString, out num1);
+num1++;
+Console.WriteLine($"User entered number + 1 {num1}");
