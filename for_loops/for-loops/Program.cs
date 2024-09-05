8e8039943ed40ac31296f5a514261442d4d8ba69
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-04 21:46:02 -0500

    Add for loop that counts down

--- for_loops/for-loops/Program.cs
+++ for_loops/for-loops/Program.cs
@@ -1,4 +1,11 @@
-﻿for (int i = 0; i < 10; i++)
+﻿// A for loop that counts up
+for (int i = 0; i < 10; i++)
 {
 	Console.WriteLine(i);
+}
+
+// A for loop that counts down
+for (int counter = 10; counter > 0; counter--)
+{
+	Console.WriteLine(counter);
 }
