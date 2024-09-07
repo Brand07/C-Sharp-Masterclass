35ab38664374f462d96f5993cba2eeb524286ea9
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-06 23:36:58 -0500

    Add While and For loop example for reference

---
+++ rocket_project/console_rocket/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * Using a string and for loop(s)
+ * Simulate a rocket landing
+*/
+
+for (int rocket = 5; rocket >= 0; rocket--)
+{
+Console.WriteLine("/\\");
+Console.WriteLine("");
+Console.WriteLine();
+Console.WriteLine("/\\");
+Thread.Sleep(1000);
+
+
+}
+Console.WriteLine("We have landed.");
---
+++ while_loops/while-loops/Program.cs
@@ -0,0 +1,17 @@
+﻿// The While Loop
+
+// For loop as an example
+Console.WriteLine("For Loop:");
+for (int i = 0; i < 10; i++)
+{
+	Console.WriteLine(i);
+}
+
+// While Loop example
+Console.WriteLine("While Loop:");
+int counter = 0;
+while (counter < 10)
+{
+	Console.WriteLine(counter);
+	counter++;
+}
