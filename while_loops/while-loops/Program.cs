e6f33670dd2fcb0aba9af129712bc11ca0ff0196
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-06 23:58:57 -0500

    Add Guess the Number game

--- while_loops/while-loops/Program.cs
+++ while_loops/while-loops/Program.cs
@@ -1,22 +1,49 @@
 ﻿// The While Loop
 
 // For loop as an example
-Console.WriteLine("For Loop:");
-for (int i = 0; i < 10; i++)
+// Console.WriteLine("For Loop:");
+// for (int i = 0; i < 10; i++)
+// {
+// 	Console.WriteLine(i);
+// }
+//
+// // While Loop example
+// Console.WriteLine("Enter 'go' or 'stay'.");
+//
+// string userChoice = Console.ReadLine();
+//
+// while (userChoice == "go")
+// {
+// 	Console.WriteLine($"I was thinking {userChoice} too!");
+// 	Console.WriteLine("Want to keep going? Enter 'go'.");
+// 	userChoice = Console.ReadLine();
+// } 
+
+// Guess the number game using while loop(s)
+
+int secretNumber = 42;
+int userGuess = 0;
+
+Console.WriteLine("Guess the number I'm thinking between 1 and 100: ");
+
+while (userGuess != secretNumber)
 {
-	Console.WriteLine(i);
+	//Console.WriteLine("Wrong. Try again.");
+	userGuess = int.Parse(Console.ReadLine());
+	if (userGuess < secretNumber)
+	{
+		Console.WriteLine("Too Low.");
+	}
+	else if (userGuess > secretNumber)
+	{
+		Console.WriteLine("Too High.");
+	}
+	else if (userGuess == secretNumber)
+	{
+		userGuess = secretNumber;
+		Console.WriteLine("Correct!");
+	}
 }
 
-// While Loop example
-Console.WriteLine("Enter 'go' or 'stay'.");
-
-string userChoice = Console.ReadLine();
-
-while (userChoice == "go")
-{
-	Console.WriteLine($"I was thinking {userChoice} too!");
-	Console.WriteLine("Want to keep going? Enter 'go'.");
-	userChoice = Console.ReadLine();
-} 
 
 
--- while_loops/while-loops/obj/Debug/net8.0/while-loops.AssemblyInfo.cs
+++ while_loops/while-loops/obj/Debug/net8.0/while-loops.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("while-loops")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ab38664374f462d96f5993cba2eeb524286ea9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ec38607aa46c2b2a9dfbb7a6b5d26eedc50e09")]
 [assembly: System.Reflection.AssemblyProductAttribute("while-loops")]
 [assembly: System.Reflection.AssemblyTitleAttribute("while-loops")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
