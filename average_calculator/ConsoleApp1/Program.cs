bdba090c44454e0152d23273ea0d000433131fae
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-16 19:59:46 -0500

    Finish calculator

--- average_calculator/ConsoleApp1/Program.cs
+++ average_calculator/ConsoleApp1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Author: Brandon Yates
+Date: 9/16/2024
+*/
+
+// Average Calculator
+
+int currentScore;
+int sum = 0;
+int counter = 0;
+
+do
+{
+	Console.WriteLine("Enter your student's scores. Enter -1 to finish.");
+	currentScore = int.Parse(Console.ReadLine());
+	if (currentScore != -1)
+	{
+		sum = sum + currentScore;
+		//Count the itterations
+		counter++;
+	}
+	
+} while (currentScore != -1);
+// Find the average score
+int average = sum / counter;
+Console.WriteLine($"Your student's average score is: {average}");
--- average_calculator/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ average_calculator/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2dc8359969b3999323e9b7f51c62566aa080868")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcc5097df90ad9057c1b5048fb202acb5357266e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
