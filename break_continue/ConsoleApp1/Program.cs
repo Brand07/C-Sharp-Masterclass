1da59d73ca72b5c7df75acbaf74a296574d76e27
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-16 20:08:56 -0500

    Example added

--- break_continue/ConsoleApp1/Program.cs
+++ break_continue/ConsoleApp1/Program.cs
@@ -2,3 +2,15 @@
 Author: Brandon Yates
 Date: 9/16/2024
 */
+
+// Break and Continue
+for (int i = 0; i < 10; i++)
+{
+	Console.WriteLine(i);
+	if (i == 3)
+	{
+		Console.WriteLine("I've had enough");
+		continue;
+	}
+	Console.WriteLine("Only if not continued");
+}
