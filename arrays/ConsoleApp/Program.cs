7669a799e29f63c817e8169ac50c2c3bf0c6379d
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-17 20:22:41 -0500

    Add indexing examples

--- arrays/ConsoleApp/Program.cs
+++ arrays/ConsoleApp/Program.cs
@@ -12,3 +12,12 @@ int num5 = 0;
 
 // Declare an array
 int[] myIntArray = new int[5];
+//At index 0, assign 5, etc
+myIntArray[0] = 5;
+myIntArray[1] = 12;
+myIntArray[2] = 15;
+myIntArray[3] = 1;
+myIntArray[4] = 34;
+
+//Access an item in the array
+Console.WriteLine(myIntArray[3]); //Should print 1 in the console
--- arrays/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ arrays/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da59d73ca72b5c7df75acbaf74a296574d76e27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730fad67409847e5d3c51258ecafeccf4243156b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
