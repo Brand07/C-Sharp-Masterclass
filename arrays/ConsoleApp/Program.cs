13cafc5d5a4aceef7a4a553a341a5f19146875c4
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-17 20:30:23 -0500

    Add foreach loop example to loop through the days of the week

--- arrays/ConsoleApp/Program.cs
+++ arrays/ConsoleApp/Program.cs
@@ -4,20 +4,21 @@ Author: Brandon Yates
 Date: 9/17/2024
 */
 
-int num1 = 0;
-int num2 = 0;
-int num3 = 0;
-int num4 = 0;
-int num5 = 0;
 
-// Declare an array
-int[] myIntArray = new int[5];
-//At index 0, assign 5, etc
-myIntArray[0] = 5;
-myIntArray[1] = 12;
-myIntArray[2] = 15;
-myIntArray[3] = 1;
-myIntArray[4] = 34;
+// Declare an array and set the array values
+int[] myIntArray = [5, 34, 34, 12, 45];
+
 
 //Access an item in the array
-Console.WriteLine(myIntArray[3]); //Should print 1 in the console
+Console.WriteLine(myIntArray[3]); //Should print 12 in the console
+
+
+//Declare an array with the days of the week
+string[] weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+//Foreach loop to loop through the days of the week
+foreach (var day in weekdays)
+{
+	Console.WriteLine($"Today is {day}");
+}
+
--- arrays/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ arrays/ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+730fad67409847e5d3c51258ecafeccf4243156b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7669a799e29f63c817e8169ac50c2c3bf0c6379d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
