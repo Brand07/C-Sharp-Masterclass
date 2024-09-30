fbd9c157930cff41d5733a12366facceb005561a
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-30 15:53:13 -0500

    Add context to Method Syntax

--- methods/methods/Program.cs
+++ methods/methods/Program.cs
@@ -4,3 +4,17 @@
 // {
 //		Method Body
 // }
+
+// -- Access Specifier - The Access Specifier determines the visibility of a variable or a method from another class.
+
+// -- Return Type - A method may return a value. The return type is the data type of the value the method returns.
+//					If the method is not returning any values, then the return type is void.
+
+// -- Method Name - Method name is a unique identifier and it is case sensitive. It cannot be the same as any other
+//					Identifier declared in the class.
+
+// -- Parameter List - Enclosed between parentheses, the parameters are used to pass and receive data from a method.
+//						The parameter list refers to the type, order, and number of the parameters of a method. 
+//						Parameters are optional; that is, a method may contain no parameters.
+
+// -- Method Body - This contains the set of instructions needed to complete the required activity.
