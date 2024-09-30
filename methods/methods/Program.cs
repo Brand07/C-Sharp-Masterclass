08baed64e41b90a20c50ba492fa05beb32985997
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-09-30 16:09:15 -0500

    Add code example

--- methods/methods/Program.cs
+++ methods/methods/Program.cs
@@ -18,3 +18,12 @@
 //						Parameters are optional; that is, a method may contain no parameters.
 
 // -- Method Body - This contains the set of instructions needed to complete the required activity.
+
+
+// EXAMPLE
+
+public int Add(int num1, int num2)
+{
+	int result = num1 + num2;
+	return result;
+}
