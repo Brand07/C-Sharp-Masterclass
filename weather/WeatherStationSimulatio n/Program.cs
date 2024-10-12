c9d154ef293b1a802593da474caab2abcf8677c6
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-11 23:37:51 -0500

    Init files

---
+++ weather/WeatherStationSimulatio n/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Security.Cryptography;
+
+namespace WeatherStationSimulation;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Enter the number of days to simulate");
+		int days = int.Parse(Console.ReadLine());
+
+		int[] temperature = new int[days];
+		string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+		string[] weatherConditions = new string[days];
+
+		Random random = new Random();
+
+		for (int i = 0; i < days; i++)
+		{
+			temperature[i] = random.Next(-10, 40);
+			weatherConditions[i] = conditions[random.Next(conditions.Length)];
+			Console.WriteLine(weatherConditions);
+			Console.WriteLine(temperature);
+		}
+	}
+}
