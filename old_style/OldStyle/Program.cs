d8f3d3f11c03dfc94f1557c84157a19ad3ea7a77
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-11 23:09:45 -0500

    Add Functions/Methods

--- old_style/OldStyle/Program.cs
+++ old_style/OldStyle/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		int myResult;
+
+		int AddTwoValues(int value1, int value2)
+		{
+			int result = value1 + value2;
+			return result;
+		}
+
+		int SubtractTwoValues(int value1, int value2)
+		{
+			int result = value1 - value2;
+			return result;
+		}
+
+		Console.WriteLine("Enter a number, and I'll add 10 to it!");
+		int num1 = int.Parse(Console.ReadLine());
+
+		myResult = AddTwoValues(num1, 10);
+		Console.WriteLine($"The result is {myResult}");
 	}
 }
