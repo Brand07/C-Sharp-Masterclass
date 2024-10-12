b8c0a390154a03fda6a80cde05ae064e63994f79
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-11 23:25:14 -0500

    Add static vs private examples

--- old_style/OldStyle/Program.cs
+++ old_style/OldStyle/Program.cs
@@ -2,28 +2,30 @@
 
 class Program
 {
+	// Global variable | field or instance variable
+	int myResult;
+
 	static void Main(string[] args)
 	{
-		int myResult;
-
-		
-
-		int SubtractTwoValues(int value1, int value2)
-		{
-			int result = value1 - value2;
-			return result;
-		}
+		Program program = new Program();
 
 		Console.WriteLine("Enter a number, and I'll add 10 to it!");
 		int num1 = int.Parse(Console.ReadLine());
 
-		myResult = AddTwoValues(num1, 10);
-		Console.WriteLine($"The result is {myResult}");
+		program.myResult = AddTwoValues(num1, 10);
+		Console.WriteLine($"The result is {program.myResult}");
 	}
-	//static allows a method to be used outside of the main method
+
+	// static allows a method to be used outside of the main method
 	static int AddTwoValues(int value1, int value2)
 	{
 		int result = value1 + value2;
+		return result;
+	}
+
+	static int SubtractTwoValues(int value1, int value2)
+	{
+		int result = value1 - value2;
 		return result;
 	}
 }
