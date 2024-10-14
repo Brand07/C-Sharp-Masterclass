09996f956491f240314ad822ad06d113ad6959bc
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-13 21:21:21 -0500

    Change properties via refactoring

--- OOP/ClassesApp/Car.cs
+++ OOP/ClassesApp/Car.cs
@@ -9,11 +9,15 @@ public class Car
 	private string _brand = "";
 	private string _model = "";
 	
+	//Property
+	public string Model { get => _model; set => _model = value; }
+	public string Brand { get => _brand; set => _brand = value; }
+	
 	//Constructor
 	public Car(string brand, string model)
 	{
 		_brand = brand;
-		_model = model;
+		Model = model;
 		Console.WriteLine($"A car with the brand of {_brand} and the model {_model} has been created.");
 	}
 }
