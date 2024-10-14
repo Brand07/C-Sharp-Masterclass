01b17a21cf233bbc6764f23ebcb652114a5c6679
Brandon Yates <81128304+Brand07@users.noreply.github.com>
2024-10-13 21:00:19 -0500

    Create new instance of the Car class

--- OOP/ClassesApp/Car.cs
+++ OOP/ClassesApp/Car.cs
@@ -1,6 +1,12 @@
 namespace ClassesApp;
 
+/*This is internal and can only be accessed within the same assembly*/
+
 public class Car
 {
-	
+	//Constructor
+	public Car()
+	{
+		Console.WriteLine("An object of Car has been created.");
+	}
 }
